b95b2a3d490540d38f04f1ae7147561770dcd94a
pavel <pavel.dudkov@icloud.com>
2021-03-29 01:50:27 +0400

    Added in view var turn_on_block. In models add model Image. In template set username by request.

--- commerce/main/views.py
+++ commerce/main/views.py
@@ -8,6 +8,7 @@ class Home(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = "Главная"
+        context['turn_on_block'] = True
         return context
 
 
