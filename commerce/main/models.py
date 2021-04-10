c44817485766f7a52f4e695bd00272c6a92ec5b1
pavel <pavel.dudkov@icloud.com>
2021-04-10 11:34:41 +0400

    When registering, the user is added to the common users group.

--- commerce/main/models.py
+++ commerce/main/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.urls import reverse
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 from main.validators import validator_age
 
 from mptt.models import MPTTModel, TreeForeignKey
@@ -112,4 +112,9 @@ class Profile(models.Model):
     def create_user_profile(sender, instance, created, **kwargs):
         if created:
             Profile.objects.create(user=instance)
+            instance.groups.add(Group.objects.get_or_create(name='common users')[0])
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
 
