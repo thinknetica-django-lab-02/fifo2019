7d3fbda69e2a7bb6741b19e16f7571722130fa1a
pavel <pavel.dudkov@icloud.com>
2021-04-28 23:19:20 +0400

    Update ProductAdmin fields and added actions by status product.

--- commerce/main/admin.py
+++ commerce/main/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.db import models
+from django.utils.safestring import mark_safe
+
 from main.models import Product, Category, Subsciber, Tag
 from django.contrib.flatpages.admin import FlatPageAdmin
 from django.contrib.flatpages.models import FlatPage
@@ -13,7 +15,27 @@ class CreditFlatPageAdmin(FlatPageAdmin):
 
 
 class ProductAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "price", "quantity", "category", "is_active", "get_html_image")
+    list_display_links = ('id', 'title')
+    fields = ("title", "short_desc", "description", "category", "tags", "image", "get_html_image", "price", "quantity", "discount", "is_active", "views")
+    readonly_fields = ('get_html_image',)
+    list_filter = ('tags',)
     exclude = ('slug',)
+    actions = ['make_is_active', 'make_not_active']
+
+    def get_html_image(self, object):
+        # mark_safe - указывает чтобы не экранировать теги
+        if object.image:
+            return mark_safe(f"<img src='{object.image.url}' width=50 height=50 style='border-radius: 30px;'>")
+    get_html_image.short_description = "Миниатюра"
+
+    def make_is_active(modeladmin, request, queryset):
+        queryset.update(is_active=True)
+    make_is_active.short_description = "Mark selected products is active"
+
+    def make_not_active(modeladmin, request, queryset):
+        queryset.update(is_active=False)
+    make_not_active.short_description = "Mark selected products not active"
 
 
 class CategoryAdmin(admin.ModelAdmin):
